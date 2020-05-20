25c5b7addafaff2dad2e04cbfcb0083a0cd1ca59
Sean Liao <seankhliao@gmail.com>
2020-05-21 01:15:45 +0200

    count build tags

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"sort"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	_ "net/http/pprof"
 
@@ -26,13 +28,13 @@ const (
 
 	chkptIndex = "index.pb"
 
-	limit = 10
+	limit = 200
 )
 
 var (
 	pool = sync.Pool{
 		New: func() interface{} {
-			b := make([]byte, 1<<29)
+			b := make([]byte, 1<<25)
 			buf := bytes.NewBuffer(b)
 			buf.Reset()
 			return buf
@@ -99,25 +101,32 @@ func BuildTags(pbi *pb.Index) {
 		}
 	}()
 
-	var i int
+	var i, gone int64
+	go func() {
+		for range time.NewTicker(10 * time.Second).C {
+			log.Printf("progress %d/%d gone %d\n", atomic.LoadInt64(&i), len(mods), atomic.LoadInt64(&gone))
+		}
+	}()
 	for m, v := range mods {
 		wg.Add(1)
 		<-sem
-		i++
-		if i%1000 == 0 {
-			log.Printf("progress %d/%d\n", i, len(mods))
-		}
+		atomic.AddInt64(&i, 1)
 		go func(m, v string) {
 			buf := pool.Get().(*bytes.Buffer)
 			defer func() {
 				buf.Reset()
 				pool.Put(buf)
 				wg.Done()
+				sem <- struct{}{}
 			}()
 
 			res, err := http.Get(fmt.Sprintf("%s/%s/@v/%s.zip", proxyURL, m, v))
 			if err != nil {
 				log.Printf("module get %s %s: %v", m, v, err)
+				return
+
+			} else if res.StatusCode == 410 {
+				atomic.AddInt64(&gone, 1)
 				return
 			} else if res.StatusCode != 200 {
 				log.Printf("module status %s %s: %d %s", m, v, res.StatusCode, res.Status)
